2b78614280185d829fb85c88e218959859d8ccf8
PanpaKawaii <dngngchitriu2004@gmail.com>
2024-10-04 23:55:44 +0700

    Add files via upload

--- HeaderFooter/Footer.jsx
+++ HeaderFooter/Footer.jsx
@@ -1,34 +1,32 @@
 import React, { Component } from 'react'
 import './Footer.css';
 
-export default class Footer extends Component {
-    render() {
-        return (
-            <div className='footer'>
-                <div className='footer-container'>
-                    <div className='footer-logo'>
-                        <h2>InnoSpace</h2>
-                        <p>InnoSpace là hệ thống quản lý và đặt chỗ làm việc cá nhân, giúp người dùng dễ dàng tìm kiếm và đặt chỗ theo nhu cầu.</p>
-                    </div>
-                    <div className='footer-location'>
-                        <h2>Location</h2>
-                        <p>+84 333 680 099</p>
-                        <p>info@workflow.com.vn</p>
-                        <p>39 Nguyen Duy Hieu, Thao Dien Ward, Thu Duc, HCMC</p>
-                    </div>
-                    <div className='footer-solution'>
-                        <h2>Solution</h2>
-                        <p>Cabin</p>
-                        <p>Phòng 1 người</p>
-                        <p>Phòng 2 người</p>
-                        <p>Phòng nhóm</p>
-                        <p>Văn phòng</p>
-                    </div>
+export default function Footer() {
+    return (
+        <div className='footer'>
+            <div className='footer-container'>
+                <div className='footer-brand'>
+                    <h3>InnoSpace</h3>
+                    <p>InnoSpace là hệ thống quản lý và đặt chỗ làm việc cá nhân, giúp người dùng dễ dàng tìm kiếm và đặt chỗ theo nhu cầu.</p>
                 </div>
-                <div className='footer-something'>
-                    <p>Copyright © 2024 Tactic</p>
+                <div className='footer-location'>
+                    <h3>Location</h3>
+                    <p>+84 333 680 099</p>
+                    <p>info@workflow.com.vn</p>
+                    <p>39 Nguyen Duy Hieu, Thao Dien Ward, Thu Duc, HCMC</p>
+                </div>
+                <div className='footer-solution'>
+                    <h3>Solution</h3>
+                    <p>Cabin</p>
+                    <p>Phòng 1 người</p>
+                    <p>Phòng 2 người</p>
+                    <p>Phòng nhóm</p>
+                    <p>Văn phòng</p>
                 </div>
             </div>
-        )
-    }
+            <div className='footer-end'>
+                <p>Copyright © 2024 Tactic</p>
+            </div>
+        </div>
+    )
 }
--- HeaderFooter/Header.jsx
+++ HeaderFooter/Header.jsx
@@ -1,25 +1,26 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import M from 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+import { Navbar, Icon } from 'react-materialize';
 import './Header.css';
 
-export default class Header extends Component {
-    render() {
-        return (
-            <div className='header'>
-                <div className='navbar'>
-                    <ul>
-                        <li className='navbar-brand'>InnoSpace</li>
-                    </ul>
-                    <ul>
-                        <li><a href='/'>Home</a></li>
-                        <li><a href='/about'>AboutUs</a></li>
-                        <li><a href='/solution'>Solution</a></li>
-                        <li><a href='/contact'>Contact</a></li>
-                    </ul>
-                    <ul>
-                        <li><a href='/'>User</a></li>
-                    </ul>
-                </div>
-            </div>
-        )
-    }
+export default function Header() {
+    return (
+        <div className='header'>
+            <Navbar className='menu'
+                alignLinks='right'
+                brand={<Link to='/' className='brand-logo'><span>InnoSpace</span></Link>}
+                id='orchids-nav'
+                menuIcon={<Icon>menu</Icon>}>
+                <ul>
+                    <li><Link to='/'><Icon left>home</Icon>Home</Link></li>
+                    <li><Link to='/about'><Icon left>info_outline</Icon>About</Link></li>
+                    <li><Link to='/solution'><Icon left>dvr</Icon>Solution</Link></li>
+                    <li><Link to='/contact'><Icon left>contacts</Icon>Contact</Link></li>
+                    <li><Link to='/user/information'><Icon left>user</Icon>User</Link></li>
+                </ul>
+            </Navbar>
+        </div>
+    )
 }
