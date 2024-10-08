44b6c9e550291b82ae666de255e5804593d8247f
PanpaKawaii <dngngchitriu2004@gmail.com>
2024-10-09 00:59:49 +0700

    Add files via upload

--- About/AboutContent.jsx
+++ About/AboutContent.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
 import './AboutContent.css'
 
-import building from '../imagePOD/building.jpg'
-import location from '../imagePOD/location.jpg'
-import videoSrc from '../imagePOD/VIDEO.mp4';
+import building from '../BackgroundImage/building.jpg'
+import location from '../BackgroundImage/location.jpg'
+import videoSrc from '../BackgroundImage/VIDEO.mp4';
 
 export default function AboutContent() {
     return (
         <div className='POD-about'>
-            <div className='about-container'>
-                <div className='video-container'>
-                    <video className='fit' width="100%" height="auto" autoPlay muted loop>
-                        <source src={videoSrc} type="video/mp4" />
-                        Your browser does not support the video tag.
-                    </video>
-                </div>
 
-                <div className='about-us-container'>
-                    <div className='slogan-container'>
-                        <p className='slogan'>Nơi làm việc lý tưởng cho sự sáng tạo và hiệu quả (logo)</p>
-                        <p className='sub-slogan'>Tự hào mang đến cho bạn giải pháp tối ưu để làm việc tại bất kỳ đâu, với không gian linh hoạt và dịch vụ chất lượng cao.</p>
-                    </div>
-                    <p className='title'>Vì sao chọn InnoSpace</p>
+            <div className='video-container'>
+                <video className='fit' autoPlay muted loop>
+                    <source src={videoSrc} type='video/mp4' />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+
+            <div className='slogan-container'>
+                <h1><b>Nơi làm việc lý tưởng cho sự sáng tạo và hiệu quả (logo)</b></h1>
+                <h3><i>Tự hào mang đến cho bạn giải pháp tối ưu để làm việc tại bất kỳ đâu, với không gian linh hoạt và dịch vụ chất lượng cao.</i></h3>
+            </div>
+
+            <div className='about-container'>
+                <div>
+                    <h4>Vì sao chọn InnoSpace</h4>
 
                     <div className='reason-container'>
-                        <div>
-                            <img className='location-img' src={location} alt='location' />
-                        </div>
+                        <img src={location} alt='location' />
                         <div>
                             <div className='reason-item'>
                                 <h3>Không gian làm việc đa dạng: </h3>
@@ -41,20 +41,18 @@ export default function AboutContent() {
                                 <p>Hỗ trợ quản lý lịch làm việc, thanh toán trực tuyến và cung cấp các gói dịch vụ linh hoạt kèm các tiện ích.</p>
                             </div>
                         </div>
-
-                    </div>
-
-                    <div className='community-container'>
-                        <div className='item-1'>
-                            <p className='second-title'>Cộng đồng và kết nối</p>
-                            <p className='community-text'>InnoSpace không chỉ là một nền tảng đặt chỗ làm việc, mà còn là một cộng đồng năng động và kết nối. Chúng tôi tạo ra một môi trường nơi những cá nhân sáng tạo, các startup và doanh nghiệp nhỏ có thể gặp gỡ, chia sẻ ý tưởng và hợp tác xây dựng một cộng đồng hỗ trợ, hợp tác và thành công.</p>
-                        </div>
-                        <div >
-                            <img className='building-img' src={building} alt='location' />
-                        </div>
                     </div>
                 </div>
+
+                    <div className='community-container'>
+                        <div className='text'>
+                            <h3>Cộng đồng và kết nối</h3>
+                            <p>InnoSpace không chỉ là một nền tảng đặt chỗ làm việc, mà còn là một cộng đồng năng động và kết nối. Chúng tôi tạo ra một môi trường nơi những cá nhân sáng tạo, các startup và doanh nghiệp nhỏ có thể gặp gỡ, chia sẻ ý tưởng và hợp tác xây dựng một cộng đồng hỗ trợ, hợp tác và thành công.</p>
+                        </div>
+                        <img className='building-img' src={building} alt='location' />
+                    </div>
             </div>
+
         </div>
     )
 }
