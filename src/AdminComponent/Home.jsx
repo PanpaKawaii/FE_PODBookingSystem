e29051c1daa194986df46dccdf9b73f7b25fddad
KimHang1504 <160225729+KimHang1504@users.noreply.github.com>
2024-10-09 11:31:00 +0700

    Update Home

--- src/AdminComponent/Home.jsx
+++ src/AdminComponent/Home.jsx
@@ -1,22 +1,20 @@
-import React, { Component } from 'react'
-import home from '../../src/PODimage/adminhome.jpg'
+import React from 'react'
+import './HomeStyle.css'
+import { Button } from 'react-bootstrap'
+import welcomeImage from '../Admin_image/avt.jpg'; // Thay thế bằng đường dẫn đến hình ảnh chào mừng
+import './HomeStyle.css'
 
-export default class Home extends Component {
-  render() {
-    return (
-      <>
-        <div 
-          className='admin-home'
-          style={{ 
-            backgroundImage: `url(${home})`, 
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            height: '100vh' 
-          }}
-        >
-          <p>Hello</p>
-        </div>
-      </>
-    )
-  }
+export default function Home() {
+  return (
+    <>
+
+      <div className="welcome-container">
+        <img src={welcomeImage} alt="Welcome" className="welcome-image" />
+        <h1>Chào mừng, [Admin Name]!</h1>
+        <p>Chúng tôi rất vui được có bạn ở đây. Hãy khám phá các chức năng bên cạnh!</p>
+       
+      </div>
+
+    </>
+  )
 }
