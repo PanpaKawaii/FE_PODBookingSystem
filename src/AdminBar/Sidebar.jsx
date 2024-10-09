579197a59ddfedd6a460456275888652d5356682
KimHang1504 <kimhangbt2017@gmail.com>
2024-10-09 11:58:58 +0700

    add more css

--- src/AdminBar/Sidebar.jsx
+++ src/AdminBar/Sidebar.jsx
@@ -9,11 +9,15 @@ import {
 } from 'cdbreact'
 import './Sidebar.css'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Thêm import cho FontAwesomeIcon
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'; // Thêm import cho icon Log out
+
+
 const Sidebar = () => {
   return (
     <CDBSidebar style={{
-       height: '100vh', // Sử dụng dấu phẩy thay vì dấu chấm phẩy
-       }} textColor="#333" backgroundColor="#f4f4f4">
+      height: '100vh', // Sử dụng dấu phẩy thay vì dấu chấm phẩy
+    }} textColor="#333" backgroundColor="#f4f4f4">
       <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>
         <Link to='/' className="sidebar-link">INNOSPACE ™</Link>
       </CDBSidebarHeader>
@@ -24,16 +28,17 @@ const Sidebar = () => {
             <Link to='/pod'>Manage POD</Link>
           </CDBSidebarMenuItem>
           <CDBSidebarMenuItem icon="sticky-note" textFontSize="14px">
-          <Link to='/user'>Manage User</Link>
+            <Link to='/user'>Manage User</Link>
           </CDBSidebarMenuItem>
-          <CDBSidebarMenuItem icon="credit-card" iconType="solid" textFontSize="14px">
+          {/* <CDBSidebarMenuItem >
             Care Products
-          </CDBSidebarMenuItem>
-          <CDBSidebarMenuItem icon="gamepad" iconType="solid" textFontSize="14px">
-          <Link to='/'>Log out</Link>
+          </CDBSidebarMenuItem> */}
+          <CDBSidebarMenuItem textFontSize="14px">
+            <FontAwesomeIcon icon={faRightFromBracket} />
+            <Link to='/'>Log out</Link>
           </CDBSidebarMenuItem>
         </CDBSidebarMenu>
-        
+
       </CDBSidebarContent>
 
       {/* Thay thế CDBSidebarCTA bằng nội dung tùy chỉnh */}
--- src/AdminComponent/Home.jsx
+++ src/AdminComponent/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './HomeStyle.css'
+import { Button } from 'react-bootstrap'
+import welcomeImage from '../Admin_image/avt.jpg'; // Thay thế bằng đường dẫn đến hình ảnh chào mừng
+import './HomeStyle.css'
 
 export default function Home() {
   return (
     <>
 
-      
-      <p>Hello, [Admin Name]</p>
+      <div className="welcome-container">
+        <img src={welcomeImage} alt="Welcome" className="welcome-image" />
+        <h1>Chào mừng, [Admin Name]!</h1>
+        <p>Chúng tôi rất vui được có bạn ở đây. Hãy khám phá các chức năng bên cạnh!</p>
+       
+      </div>
 
     </>
   )
