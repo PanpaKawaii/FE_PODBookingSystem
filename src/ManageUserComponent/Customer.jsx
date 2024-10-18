4917a9626bd2e7bee4f79b08a2bfe84d1d256b2f
KimHang1504 <kimhangbt2017@gmail.com>
2024-10-18 22:01:15 +0700

    add data of staff and pod

--- src/ManageUserComponent/Customer.jsx
+++ src/ManageUserComponent/Customer.jsx
@@ -1,43 +1,59 @@
 import React from 'react';
 import './UserManage.css'; // Thêm CSS cho component
-import avt from '../Admin_image/avt.jpg'
 import Button from 'react-bootstrap/Button'; // Thêm import cho Button
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'; // Thêm import cho biểu tượng
+import { useState, useEffect } from 'react';
+import api from '../api/axios';
+import { Row } from 'react-bootstrap';
+import avatar from '../Admin_image/avt.jpg';
 const Customer = () => {
-    const users = [
-        { id: '123456', class: 'SWR-9', position: 'Event Manager', name: 'Sarah', avatar: avt },
-        { id: '123457', class: 'SWR-9', position: 'Event Manager', name: 'John', avatar: avt },
-        { id: '123458', class: 'SWR-9', position: 'Event Manager', name: 'Alice', avatar: avt },
-        { id: '123459', class: 'SWR-9', position: 'Event Manager', name: 'Bob', avatar: avt },
-    ];
+    const [user, setUser] = useState([]);
+    const fetchUser = async () => {
+        try {
+            const response = await api.get("User");
+            const filteredUsers = response.data.filter(user => user.role === "User");
+            setUser(filteredUsers);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    useEffect(() => {
+        fetchUser();
+    }, []);
 
     return (
-        <div className="user-manage">
-            <h1>Tài khoản khách hàng</h1>
-            <div className="user-list">
-                {users.map((user, index) => (
-                    <div key={index} className="user-card">
-                        <img src={user.avatar} alt={`${user.name}'s avatar`} className="user-avatar" />
-                        <div className="user-info">
-                            <h3>{user.name} !</h3>
-                            <p>ID: {user.id}</p>
-                            <p>CLASS: {user.class}</p>
-                            <p>POSITION: {user.position}</p>
+        <>
+            <Row >
+            <div className="user-manage">
+                <h1>Tài khoản khách hàng</h1>
+                <div className="user-list">
+                    {user.map((user, index) => (
+                        <div key={index} className="user-card">
+                            <img src={avatar} alt={`${user.name}'s avatar`} className="user-avatar" />
+                            <div className="user-info">
+                                <h3>{user.name} !</h3>
+                                <p><strong>ID:</strong> {user.id}</p>
+                                <p><strong>Tên:</strong> {user.name}</p>
+                                <p><strong>Email:</strong> {user.email}</p>
+                                <p><strong>Số điện thoại:</strong> {user.phoneNumber}</p>
+                                <p><strong>Loại tài khoản:</strong> {user.type}</p>
+                                <p><strong>Điểm thưởng:</strong> {user.point}</p>
+                            </div>
+                            <div className="user-actions">
+                                    <Button variant onClick={() => handleEdit(idx)}>
+                                    <FontAwesomeIcon icon={faEdit} /> {/* Biểu tượng sửa */}
+                                </Button>
+                                <Button variant onClick={() => handleDelete(idx)}>
+                                    <FontAwesomeIcon icon={faTrash} /> {/* Biểu tượng xóa */}
+                                </Button>
+                            </div>
                         </div>
-                        <div className="user-actions">
-                        <Button variant onClick={() => handleEdit(idx)}>
-                    <FontAwesomeIcon icon={faEdit} /> {/* Biểu tượng sửa */}
-                  </Button>
-                  <Button variant onClick={() => handleDelete(idx)}>
-                    <FontAwesomeIcon icon={faTrash} /> {/* Biểu tượng xóa */}
-                  </Button>
-                        </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
-            <button className="add-button">+</button>
-        </div>
+        </Row >
+        </>
     );
 };
 
--- src/ManageUserComponent/Staff.jsx
+++ src/ManageUserComponent/Staff.jsx
@@ -1,43 +1,61 @@
 import React from 'react';
 import './UserManage.css'; // Thêm CSS cho component
-import avt from '../Admin_image/avt.jpg'
 import Button from 'react-bootstrap/Button'; // Thêm import cho Button
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'; // Thêm import cho biểu tượng
+import { useState, useEffect } from 'react';
+import api from '../api/axios';
+import { Row } from 'react-bootstrap';
+import avatar from '../Admin_image/avt.jpg';
+import { Link } from 'react-router-dom';
 const Staff = () => {
-    const users = [
-        { id: '123456', class: 'SWR-9', position: 'Event Manager', name: 'Sarah', avatar: avt },
-        { id: '123457', class: 'SWR-9', position: 'Event Manager', name: 'John', avatar: avt },
-        { id: '123458', class: 'SWR-9', position: 'Event Manager', name: 'Alice', avatar: avt },
-        { id: '123459', class: 'SWR-9', position: 'Event Manager', name: 'Bob', avatar: avt },
-    ];
+    const [staff, setStaff] = useState([]);
+    const fetchStaff = async () => {
+        try {
+            const response = await api.get("User");
+            const filteredUsers = response.data.filter(user => user.role === "Staff");
+            setStaff(filteredUsers);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    useEffect(() => {
+        fetchStaff();
+    }, []);
 
     return (
-        <div className="user-manage">
-            <h1>Tài khoản nhân viên</h1>
-            <div className="user-list">
-                {users.map((user, index) => (
-                    <div key={index} className="user-card">
-                        <img src={user.avatar} alt={`${user.name}'s avatar`} className="user-avatar" />
-                        <div className="user-info">
-                            <h3>{user.name} !</h3>
-                            <p>ID: {user.id}</p>
-                            <p>CLASS: {user.class}</p>
-                            <p>POSITION: {user.position}</p>
+        <>
+            <Row >
+            <div className="user-manage">
+                <h1>Tài khoản nhân viên</h1>
+                <div className="user-list">
+                    {staff.map((staff, index) => (
+                        <div key={index} className="user-card">
+                            <img src={avatar} alt={`${staff.name}'s avatar`} className="user-avatar" />
+                            <div className="user-info">
+                                <h3>{staff.name} !</h3>
+                                <p><strong>ID:</strong> {staff.id}</p>
+                                <p><strong>Tên:</strong> {staff.name}</p>
+                                <p><strong>Email:</strong> {staff.email}</p>
+                                <p><strong>Số điện thoại:</strong> {staff.phoneNumber}</p>
+                                <p><strong>Loại tài khoản:</strong> {staff.type}</p>
+                                <p><strong>Điểm thưởng:</strong> {staff.point}</p>
+                            </div>
+                            <div className="user-actions">
+                                <Button variant onClick={() => handleEdit(idx)}>
+                                    <FontAwesomeIcon icon={faEdit} /> {/* Biểu tượng sửa */}
+                                </Button>
+                                <Button variant onClick={() => handleDelete(idx)}>
+                                    <FontAwesomeIcon icon={faTrash} /> {/* Biểu tượng xóa */}
+                                </Button>
+                            </div>
                         </div>
-                        <div className="user-actions">
-                        <Button variant onClick={() => handleEdit(idx)}>
-                    <FontAwesomeIcon icon={faEdit} /> {/* Biểu tượng sửa */}
-                            </Button>
-                            <Button variant onClick={() => handleDelete(idx)}>
-                                <FontAwesomeIcon icon={faTrash} /> {/* Biểu tượng xóa */}
-                            </Button>
-                        </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
+                <button className="add-button"><Link to="/addStaff">+</Link></button>
             </div>
-            <button className="add-button">+</button>
-        </div>
+        </Row >
+        </>
     );
 };
 
