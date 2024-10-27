9d95cfbf449220f0e4a38498d90673810ccf6f57
PanpaKawaii <dngngchitriu2004@gmail.com>
2024-10-28 03:32:57 +0700

    Add Images And Main Files

--- App.jsx
+++ App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Sidebar from './SideBar/UserSideBar'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'materialize-css/dist/css/materialize.min.css';
+// import 'materialize-css/dist/css/materialize.min.css';
 import { Route, Routes } from 'react-router-dom'
+
+
+import GuestHeader from './HeaderFooter/GuestHeader'
 
 import Header from './HeaderFooter/Header'
 import Footer from './HeaderFooter/Footer'
@@ -20,16 +21,97 @@ import PaymentContent from './Payment/PaymentContent'
 
 import AboutContent from './About/AboutContent'
 
-import UserInformation from './UserControlCenter/UserControlContent/UserInformation'
-import UserAccount from './UserControlCenter/UserControlContent/UserAccount'
-import UserHistoryBooking from './UserControlCenter/UserControlContent/UserHistoryBooking'
-
+import UserInformation from './UserControlContent/UserInformation'
+import UserBooking from './UserControlContent/UserBooking'
+import UserBookingDetail from './UserControlContent/UserBookingDetail'
 import SignInSignUp from './SignInSignUp/SignInSignUp'
 
+import ScrollToTop from './ScrollToTopComponent/ScrollToTop';
+
 function App() {
-  return (
-    <>
-      <Header />
+  const UserRole = localStorage.getItem('UserRole');
+  // if (UserRole === null) {
+  //   return (
+  //     <>
+  //       <ScrollToTop />
+  //       <div className='user-container'>
+  //         <Sidebar className='sidebar' />
+  //       </div>
+  //     </>
+  //   )
+  // }
+  if (UserRole === 'Admin') {
+    return (
+      <>
+        <ScrollToTop />
+        <div className='admin-container'>
+          <Sidebar className='sidebar' />
+          This is Admin
+        </div>
+      </>
+    )
+  }
+  if (UserRole === null) {
+    return (
+      <>
+        <ScrollToTop />
+        <div>
+          <GuestHeader />
+          <div>
+            <Routes>
+              <Route path='/' element={<HomeContent />} />
+              <Route path='/about' element={<AboutContent />} />
+
+              <Route path='/booking/store' element={<BookingStoreContent />} />
+              <Route path='/booking/store/:Id' element={<BookingStoreDetailContent />} />
+              <Route path='/booking/pod' element={<BookingPodContent />} />
+              <Route path='/booking/pod/:Id' element={<BookingPodDetailContent />} />
+              <Route path='/booking/pod/:Id/payment' element={<PaymentContent />} />
+
+              <Route path='/contact' element={<ContactContent />} />
+
+              <Route path='/signinsignup' element={<SignInSignUp />} />
+            </Routes>
+            <Footer />
+          </div>
+        </div>
+      </>
+    )
+  }
+  else if (UserRole === 'User') {
+    return (
+      <>
+        <ScrollToTop />
+        <div className='user-container'>
+          <Sidebar className='sidebar' />
+          <div className='user-content' >
+            <Header />
+            <div className='user-content-body'>
+              <Routes>
+                <Route path='/' element={<HomeContent />} />
+                <Route path='/about' element={<AboutContent />} />
+
+                <Route path='/booking/store' element={<BookingStoreContent />} />
+                <Route path='/booking/store/:Id' element={<BookingStoreDetailContent />} />
+                <Route path='/booking/pod' element={<BookingPodContent />} />
+                <Route path='/booking/pod/:Id' element={<BookingPodDetailContent />} />
+                <Route path='/booking/pod/:Id/payment' element={<PaymentContent />} />
+
+                <Route path='/contact' element={<ContactContent />} />
+
+
+                <Route path='/user/information' element={<UserInformation />} />
+                <Route path='/user/booking' element={<UserBooking />} />
+                <Route path='/user/booking/:Id' element={<UserBookingDetail />} />
+
+                <Route path='/signinsignup' element={<SignInSignUp />} />
+              </Routes>
+              <Footer />
+            </div>
+          </div>
+        </div>
+
+        {/* <Header />
       <Routes>
         <Route path='/' element={<HomeContent />} />
         <Route path='/about' element={<AboutContent />} />
@@ -48,11 +130,11 @@ function App() {
         <Route path='/user/bookinghistory' element={<UserHistoryBooking />} />
 
         <Route path='/signinsignup' element={<SignInSignUp />} />
-        {/* <Route path='/GetDataAPI' element={<GetDataAPI />} /> */}
       </Routes>
-      <Footer />
-    </>
-  )
+      <Footer /> */}
+      </>
+    )
+  }
 }
 
 export default App
